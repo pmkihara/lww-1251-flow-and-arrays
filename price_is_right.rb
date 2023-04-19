8faa3696827969e10c2a6b5ee5fd5b4343ef1960
Patricia Kihara <patricia.kihara@outlook.com>
2023-04-19 20:57:30 -0300

    Manter em um loop while

--- price_is_right.rb
+++ price_is_right.rb
@@ -1,24 +1,25 @@
-# Pegar um valor randômico no range de 1..5
-price = rand(1..5)
+# Para deixar o programa rodando até o usuário parar
+running = true
 
-# Perguntar a tentativa
-puts 'Guess the price (1 to 5)'
-print '> '
+while running
+  # Pegar um valor randômico no range de 1..5
+  price = rand(1..5)
+  guess = nil
 
-# Pegar a resposta do usuário e guardar numa variável
-guess = gets.chomp.to_i
+  # Ficar no loop até o usuário acertar o preço
+  until guess == price || !running # igual a while guess != price
+    # Perguntar a tentativa
+    puts 'Guess the price (1 to 5) or exit'
+    print '> '
+    # Pegar a resposta do usuário e guardar numa variável
+    guess = gets.chomp
 
-# Enquanto a tentativa for diferente do preço:
-# while guess != price
-#   puts 'Try again'
-#   guess = gets.chomp.to_i
-# end
+    running = false if guess == 'exit'
+    guess = guess.to_i
 
-# Até que a tentativa seja igual ao preço
-until guess == price
-  puts 'Try again'
-  guess = gets.chomp.to_i
+    print 'Try again! ' unless !running || guess == price
+  end
+
+  # Printar que o usuário ganhou quando acertar o preço
+  puts running ? 'You win!' : 'Goodbye!'
 end
-
-# Printar que o usuário ganhou quando acertar o preço
-puts 'You win!'
